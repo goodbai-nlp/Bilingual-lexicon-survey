e0d30cf7dfef71dfbd9bf00ede2d23d24370fb63
muyeby <bxf_hit@163.com>
2018-04-03 23:48:23 +0800

    change run.sh

--- muse2/run.sh
+++ muse2/run.sh
@@ -2,6 +2,6 @@ for((index=32;index<=256;index*=2))
 do
 	for((i=0; i<8; ++i))
 	do 
-		
+		python3 unsupervised.py --src_emb data/word2vec.zh --tgt_emb data/word2vec.en --number $i --batch_size $index 
 	done
 done	
